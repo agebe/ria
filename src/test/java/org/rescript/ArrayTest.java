8eb78112c688dbbdd287a7f3198f1f48b979d246
Andre Gebers <andre.gebers@gmail.com>
2022-10-31 05:10:53 +1000

    cleanup

--- src/test/java/org/rescript/ArrayTest.java
+++ src/test/java/org/rescript/ArrayTest.java
@@ -5,8 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.Arrays;
-
 import org.junit.jupiter.api.Test;
 
 public class ArrayTest {
