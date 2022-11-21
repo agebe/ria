e3299d02db0a57498845dbf2f05a3527b40cf3c1
Andre Gebers <andre.gebers@gmail.com>
2022-11-22 04:58:32 +1000

    added swing border demo example

---
+++ src/test/java/org/rescript/example/SwingBorderDemo.java
@@ -0,0 +1,11 @@
+package org.rescript.example;
+
+import org.rescript.Script;
+
+public class SwingBorderDemo {
+
+  public static void main(String[] args) {
+    new Script().run(ResourceUtil.resourceAsString("SwingBorderDemo.bs"));
+  }
+
+}
